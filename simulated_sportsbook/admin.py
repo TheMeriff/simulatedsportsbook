2cb3a3136489f91e4c056f3a7093e0ee5542d8e9
meriff <themeriff@garlichurch.com>
2021-11-20 01:02:55 -0600

    admin cleanup

--- simulated_sportsbook/admin.py
+++ simulated_sportsbook/admin.py
@@ -5,14 +5,17 @@ from simulated_sportsbook.models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    fields = ['sport', 'home_team', 'away_team', 'start_time', 'spread_away_team_points', 'spread_away_team_price',
-              'spread_home_team_points', 'spread_home_team_price', 'over_under_points', 'over_price', 'under_price',
-              'away_team_money_line_price', 'home_team_money_line_price', 'last_updated', 'external_sportsbook']
+    fields = ['sport', 'home_team', 'away_team', 'start_time', 'last_updated', 'spread_away_team_points',
+              'spread_away_team_price', 'spread_home_team_points', 'spread_home_team_price', 'over_under_points',
+              'over_price', 'under_price', 'away_team_money_line_price', 'home_team_money_line_price',
+              'external_sportsbook'
+              ]
     list_display = [
-        'id', 'sport', 'home_team', 'away_team', 'start_time', 'over_under_points', 'spread_away_team_points', 'spread_away_team_price',
-              'spread_home_team_points', 'spread_home_team_price', 'external_sportsbook',
-              'away_team_money_line_price', 'home_team_money_line_price', 'last_updated']
-    list_filter = ['sport', 'start_time', 'over_under_points', 'external_sportsbook', 'spread_home_team_points']
+        'id', 'sport', 'home_team', 'away_team', 'start_time', 'over_under_points', 'spread_away_team_points',
+        'spread_away_team_price', 'spread_home_team_points', 'spread_home_team_price', 'external_sportsbook',
+        'away_team_money_line_price', 'home_team_money_line_price', 'last_updated'
+    ]
+    list_filter = ['sport', 'start_time', 'external_sportsbook']
     ordering = ['sport', 'home_team', 'away_team']
     search_fields = ['home_team', 'away_team']
     filter_horizontal = ()
