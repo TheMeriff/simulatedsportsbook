b6f5e4d23891e77df2d60e3ee049343a69e35304
Sam <themeriff@garlichurch.com>
2022-03-26 18:15:57 -0500

    time update for potty

--- simulated_sportsbook/services/telegram_service.py
+++ simulated_sportsbook/services/telegram_service.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 
 
@@ -6,13 +8,19 @@ class TelegramService:
         pass
 
     def send_potty_reminder(self):
-        bot_token = '550737128:AAFfDfWekynT_wxsQuNpynXkBO4xkdr4QNo'
-        bot_chatID = '-496578899'
+        acceptable_hours = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 1, 2]
+        now = datetime.utcnow()
+        if now.hour in acceptable_hours:
+            bot_token = '550737128:AAFfDfWekynT_wxsQuNpynXkBO4xkdr4QNo'
+            bot_chatID = '-496578899'
 
-        url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&text=' + "Does Madeline need to go potty?"
+            url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&text=' + "Does Madeline need to go potty?"
 
-        r = requests.get(url)
-        if r.status_code == 200:
-            print('telegram message sent successfully')
+            r = requests.get(url)
+
+            if r.status_code == 200:
+                print('telegram message sent successfully')
+            else:
+                print('big problems in telegram world')
         else:
-            print('big problems in telegram world')
+            print(f'{now.hour}:{now.minute} not in acceptable hours.')
