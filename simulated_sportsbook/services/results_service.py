a475a691265a56a16a55306d1229b17681137437
Sam <sam@soap-bx.com>
2023-02-03 14:11:55 -0600

    WIP NBA update part 1

--- simulated_sportsbook/services/results_service.py
+++ simulated_sportsbook/services/results_service.py
@@ -16,7 +16,9 @@ from simulated_sportsbook.tests.fixtures.nhl_fixture import nhl_team_abbreviatio
 
 class ResultsService:
     def __init__(self):
-        pass
+        self.session = requests.session()
+        possible_api_keys = []
+        self.session.headers.update({'X-RapidAPI-Key': possible_api_keys[1]})
 
     @staticmethod
     def process_nfl_events():
@@ -153,72 +155,64 @@ class ResultsService:
     @staticmethod
     def process_nba_events():
         updated_events = []
+        date_game_map = {}
         now = datetime.utcnow()
         date_filter = now - timedelta(hours=5)
         nba_events = Event.objects.filter(sport=Event.NBA).exclude(start_time__gte=date_filter).exclude(completed=True)
+        for event in nba_events:
+            if event.start_time.day < 10:
+                day = f'0{event.start_time.day}'
+            else:
+                day = event.start_time.day
+
+            if event.start_time.month < 10:
+                month = f'0{event.start_time.month}'
+            else:
+                month = event.start_time.month
+            formatted_date_range = f'{event.start_time.year}-{month}-{day}'
+            if not date_game_map.get(formatted_date_range):
+                date_game_map[formatted_date_range] = []
+
+            date_game_map[formatted_date_range].append(event)
+
         if nba_events:
-            for event in nba_events:
-                updated_event = ResultsService.get_nba_result(event)
-                updated_events.append(updated_event)
-                print(f'Updated {len(updated_events)} NBA events with scores and marked them as complete.')
+            updated_events = ResultsService().get_nba_results(date_game_map)
+            Event.objects.bulk_update(updated_events, fields=(
+                'away_team_points_scored',
+                'completed',
+                'home_team_points_scored',
+                'last_updated'
+            ))
+            print(f'Updated {len(updated_events)} NBA events with scores and marked them as complete.')
         else:
             print('No NBA games to update')
 
-    @staticmethod
-    def get_nba_result(event):
-        away_team_abbreviation = nba_team_abbreviations[event.away_team]
-        home_team_abbreviation = nba_team_abbreviations[event.home_team]
+    def get_nba_results(self, date_game_map):
+        self.session.headers.update({'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'})
+        updated_events = []
 
-        if event.start_time.day < 10:
-            day = f'0{event.start_time.day}'
-        else:
-            day = event.start_time.day
-
-        if event.start_time.month < 10:
-            month = f'0{event.start_time.month}'
-        else:
-            month = event.start_time.month
-
-        date_for_url = f'{event.start_time.year}{month}{day}'
-        r = requests.get(f'http://data.nba.net/10s/prod/v1/{date_for_url}/scoreboard.json')
-        if r.status_code == 200:
-            data = r.json()
-            games = data['games']
-            for game in games:
-                if game['vTeam']['triCode'] == away_team_abbreviation and game['hTeam']['triCode'] == home_team_abbreviation:
-                    if game['hTeam']['score'] != '' and game['vTeam']['score'] != '':
-                        event.home_team_points_scored = game['hTeam']['score']
-                        event.away_team_points_scored = game['vTeam']['score']
-                        event.completed = True
-                        event.save()
-                        print(f'{event} was successfully updated with a score.')
-                        if int(event.away_team_points_scored) > int(event.home_team_points_scored):
-                            winning_team = event.away_team
-                            higher_num = event.away_team_points_scored
-                            lower_num = event.home_team_points_scored
-                        elif int(event.home_team_points_scored) > int(event.away_team_points_scored):
-                            winning_team = event.home_team
-                            higher_num = event.home_team_points_scored
-                            lower_num = event.away_team_points_scored
-                        else:
-                            winning_team = 'Shit broke got a problem yo.'
-                            higher_num = 'Shit broke got a problem yo.'
-                            lower_num = 'Shit broke got a problem yo.'
-                        score_data = f"{event.away_team} at {event.home_team}"
-                        DiscordService().post_score(score_data, '918755540736307221')
-                        time.sleep(1)
-                        score_data_2 = f"Winner: {winning_team}"
-                        DiscordService().post_score(score_data_2, '918755540736307221')
-                        time.sleep(1)
-                        score_data_3 = f"{higher_num}-{lower_num}"
-                        DiscordService().post_score(score_data_3, '918755540736307221')
-                        time.sleep(1)
-                        score_data_4 = '--------------------------------------'
-                        DiscordService().post_score(score_data_4, '918755540736307221')
-
-                    return event
-        else:
-            raise Exception
+        try:
+            for date in date_game_map.keys():
+                url = f'https://api-nba-v1.p.rapidapi.com/games?date={date}'
+                # For rate limit
+                time.sleep(10)
+                r = self.session.get(url)
+                data = r.json()['response']
+                print(f"\n Remaining daily requests {r.headers['x-ratelimit-requests-remaining']} \n")
+                for game in data:
+                    away_team = game['teams']['visitors']['name']
+                    home_team = game['teams']['home']['name']
+                    for event in date_game_map[date]:
+                        if all((away_team == event.away_team, home_team == event.home_team)):
+                            event.away_team_points_scored = game['scores']['visitors']['points']
+                            event.home_team_points_scored = game['scores']['home']['points']
+                            event.last_updated = datetime.utcnow()
+                            event.completed = True
+                            updated_events.append(event)
+                            print(f'Updated {event}')
+        except Exception as e:
+            print(f'Error updating NBA games. | {e}')
+        return updated_events
 
     @staticmethod
     def process_nhl_events():
